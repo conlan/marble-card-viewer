14810899250c09f3c627c9ba37494162b1ecb6b5
conlan <conlan@gmail.com>
2019-09-22 17:36:40 -0700

    Updates

---
+++ precache-manifest.78bfb218ce2fd676b9c0fbad1ed389c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237c1292e6c925c9583f9d7bf9db1ca6",
+    "url": "/marble-card-viewer/index.html"
+  },
+  {
+    "revision": "56731ff35d02cdc16479",
+    "url": "/marble-card-viewer/static/css/main.474256c8.chunk.css"
+  },
+  {
+    "revision": "4ecebd4247876574d500",
+    "url": "/marble-card-viewer/static/js/2.d5f3c43b.chunk.js"
+  },
+  {
+    "revision": "56731ff35d02cdc16479",
+    "url": "/marble-card-viewer/static/js/main.76ae22f1.chunk.js"
+  },
+  {
+    "revision": "f7604f19d5eb0594eeb4",
+    "url": "/marble-card-viewer/static/js/runtime-main.4bda6b1d.js"
+  }
+]);
